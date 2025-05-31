def44a16b81bbb8836bf21218d31e8dbaaf6c05b
Suryamani Gupta <manigupta28201@gmail.com>
2025-05-31 11:41:55 +0530

    jwt web token authentication completed , next refresh token

--- jwt_token_auth/server.js
+++ jwt_token_auth/server.js
@@ -1,7 +1,49 @@
-import express from 'express';
+import express from 'express'
 import 'dotenv/config'  //dotenv file configuration
+import jwt from 'jsonwebtoken' 
+
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+const secretKey = "secretKey";
 
-app.listen(PORT,()=>console.log(`App is running on port ${PORT}`));
+//routes
+app.get('/', (req, res) => {
+    res.send('Get api Web token auth');
+});
+
+app.post('/logIn',(req,res)=>{
+    console.log(req.body,'reqq');
+    
+    const user = {
+        id:2,
+        name:"Mani101",
+        email:"test@gmail.com"
+    };
+    jwt.sign({user},secretKey,{expiresIn:'300s'},(err,token)=>{
+        if(err) res.status(500).json({msg:"Error in token generation"});
+        res.json({token});
+    });
+});
+
+app.post('/profile',verifyToken,(req,res)=>{
+    //verifying token
+    jwt.verify(req.token,secretKey,(err,authData)=>{
+        if(err) return res.send({result:"Invalid Token"});
+        res.json({message:"Profile accessed",authData});
+    });
+});
+
+function verifyToken(req,res,next){
+    //extracting token from headers.
+    const bearerHeader = req.headers['jwt_token'];
+    if (!bearerHeader) {
+        return res.status(500).json({msg:"Header Token is missing"});
+    };
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];   //!["Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."]
+    req.token = token;
+    next();
+};
+
+app.listen(PORT,()=>console.log(`http://localhost:${PORT}`));

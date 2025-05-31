782572ac70e206f1a0d917e6f1c142dd8f8683ec
Suryamani Gupta <manigupta28201@gmail.com>
2025-05-31 08:27:55 +0530

    New added

--- jwt_token_auth/server.js
+++ jwt_token_auth/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import 'dotenv/config'  //dotenv file configuration
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-
 
 app.listen(PORT,()=>console.log(`App is running on port ${PORT}`));
